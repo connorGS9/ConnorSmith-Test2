62e3ea266c10833f334b49142129f81c958a1121
connorGS9 <cs36356@georgiasouthern.edu>
2024-11-16 18:21:01 -0500

    Created test case for testing inventory updating

--- test2/src/CustomerOrderTest.java
+++ test2/src/CustomerOrderTest.java
@@ -39,5 +39,15 @@ public void setUp() {
         assertTrue(order.isPaid());
         assertEquals(10.00, order.getTotalAmount(), 0.01);
     }
+
+    // ToDo: Test inventory update when an order is placed
+    // Add an item to the order and reduce its stock. Verify that the inventory count is updated correctly.
+
+    @Test
+    public void testInventoryUpdate() {
+        order.addItem("Pepperoni Pizza", 12.50);
+        inventory.updateStock("Pepperoni Pizza", 1);
+        assertEquals(9, inventory.getItemStock("Pepperoni Pizza"));
+    }
 }
 
